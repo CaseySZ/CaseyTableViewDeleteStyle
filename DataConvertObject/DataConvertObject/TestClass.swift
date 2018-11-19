2c5f8f41db1e2a7403a518e18a5541481dc3266d
Casey <Casey@iv66.net>
2018-11-19 08:46:31 +0800

    init
    init

---
+++ DataConvertObject/DataConvertObject/TestClass.swift
@@ -0,0 +1,15 @@
+//
+//  TestClass.swift
+//  DataConvertObject
+//
+//  Created by Casey on 16/11/2018.
+//  Copyright © 2018 Casey. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+    
+    
+    
+}
---
+++ DataConvertObject/DataConvertObject/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  DataConvertObject
+//
+//  Created by Casey on 16/11/2018.
+//  Copyright © 2018 Casey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        convertObject()
+    }
+    
+    
+    func convertObject()  {
+        
+        let dictionary = ["test":"valueOne", "name":"casey", "department":"newPlatform"]
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
